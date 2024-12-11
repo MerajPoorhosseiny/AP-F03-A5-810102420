66d68943c1cba91fb06aeb06a05bb4658a5535fe
Amir18393 <amir3moh3amm1ad8mohos@gmail.com>
2024-12-11 20:15:48 +0330

    Update system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -1,1 +1,19 @@
+#include "system.hpp"
 
+System::System(int width, int height)
+{
+  window.create(VideoMode(width, height), "PVZ", Style::Default);
+  window.setFramerateLimit(FRAME_RATE);
+  state = MENU;
+  mode = Day;
+  set_background_texture();
+  set_gameover_texture();
+  set_menu_texture();
+  if (!music.openFromFile(AUDIO_PATH + "bg.ogg"))
+  {
+    debug("failed to load music");
+  }
+  music.setLoop(true);
+  music.play();
+  menu_handler = new Menu_Handler();
+}
