a5898533afc848872ab50c405867518f5943f7e4
Amir18393 <amir3moh3amm1ad8mohos@gmail.com>
2024-12-11 20:33:30 +0330

    Update player.hpp

--- include/player.hpp
+++ include/player.hpp
@@ -5,3 +5,16 @@ enum player_side
     left,
     right,
 };
+
+class Health{
+public:
+    Health(player_side side);
+    ~Health();
+    void health_update(int health);
+    void health_render(RenderWindow &window);
+private:
+    Texture texture;
+    Sprite sprite;
+    Font font;
+    Text text;
+};
