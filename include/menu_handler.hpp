8f981ca7941ccf991c1f8df678dc76128496ff0b
Amir18393 <amir3moh3amm1ad8mohos@gmail.com>
2024-12-11 20:46:09 +0330

    Update menu_handler.hpp

--- include/menu_handler.hpp
+++ include/menu_handler.hpp
@@ -1,3 +1,24 @@
 #pragma once
 
 #include "global.hpp"
+
+class Menu_Handler{
+public:
+    Menu_Handler ();
+    ~Menu_Handler();
+    void update(Vector2i mousePos);
+    void render(RenderWindow &window);
+    bool check_start();
+    bool check_exit();
+    bool check_mode();
+private:
+    Texture StartKeyTexture;
+    Sprite StartKeySprite;
+    bool StartKey;
+    Texture ExitKeyTexture;
+    Sprite ExitKeySprite;
+    bool ExitKey;
+    Texture ModeKeyTexture;
+    Sprite ModeKeySprite;
+    bool ModeKey;
+};
