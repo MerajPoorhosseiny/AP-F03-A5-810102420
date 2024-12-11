5ad507f56a5455684379e43da38f669724b4af8b
Amir18393 <amir3moh3amm1ad8mohos@gmail.com>
2024-12-11 20:11:48 +0330

    Update system.hpp

--- include/system.hpp
+++ include/system.hpp
@@ -1,1 +1,20 @@
+#pragma once
+
+#include "global.hpp"
+#include "setting.hpp"
+#include "game_handler.hpp"
+#include "menu_handler.hpp"
+
+enum State {
+  GAME,
+  // PAUSE,
+  MENU,
+  GAMEOVER_SCREEN,
+  EXIT
+};
+
+enum Mode{
+  Day,
+  Night
+};
 
